db8a5527bc3b9ee6a378fb6faf515e0b99e92611
IsHPDuwu <IsHPDuwu@outlook.com>
2024-11-25 01:17:17 +0800

    async -> "" fix int2str add token2str

--- make_req_text.py
+++ make_req_text.py
@@ -2,7 +2,7 @@ import random
 
 endpoint = ""
 
-async def int2str(x:int,dig:int)->str:
+def int2str(x:int,dig:int=1)->str:
     text:str = ""
     num:list[int] = []
     while(x):
@@ -14,10 +14,20 @@ async def int2str(x:int,dig:int)->str:
         text+=chr(each)
     return text
 
-async def pong() -> str:
+def token2str(x:str):
+    text:str = ""
+    x = x.replace("-","")
+    for i in range(0,32,2):
+        text+=chr(int(x[i:i+2],16))
+    return text
+
+def pong() -> str:
     return "\xfb"
 
-async def paint(x:int,y:int,color:tuple[int,int,int],uid:int,token:str) -> str:
+def paint(x:int,y:int,color:tuple[int,int,int],uid:int,token:str) -> str:
     id = random.randint(0,65535)
-    text = "\xfe"+await int2str(x)+await int2str(y)+await int2str(color[0])+await int2str(color[1])+await int2str(color[2])+await int2str(uid)+token+await int2str(id)
+    text = "\xfe"+int2str(x,2)+int2str(y,2)+int2str(color[0])+int2str(color[1])+int2str(color[2])+int2str(uid,3)+token2str(token)+int2str(id,2)
     return id,text
+# 00000000-0000-0000-0000-000000000000
+
+print(1)
