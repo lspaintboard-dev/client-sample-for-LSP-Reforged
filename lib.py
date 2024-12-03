610b20e956bca26b3a38b69c37aa3a1ae9f3e1c8
IsHPDuwu <IsHPDuwu@outlook.com>
2024-12-03 23:48:17 +0800

    完成了最简单版本的轮子
    to-do : 发包粘包

--- lib.py
+++ lib.py
@@ -22,29 +22,73 @@ async def unpack(raw:bytes):
         i+=offset
     return packages
 
-    
 
-def int2str(x:int,dig:int=1)->str:
-    text:str = ""
+def int2list(x:int,dig:int=1)->list[int]:
+    data:list[int] = []
     for i in range(0,dig):
-        text+=chr(x & 0xff)
+        data.append(x & 0xff)
         x >>= 8
-    return text
+    return data
 
-def token2str(x:str):
-    text:str = ""
+def token2str(x:str)->list[int]:
+    data:list[int] = []
     x = x.replace("-","")
     for i in range(0,32,2):
-        text+=chr(int(x[i:i+2],16))
-    return text
+        data.append(int(x[i:i+2],16))
+    return data
 
 async def pong() -> bytes:
     return b"\xfb"
 
-def paint(x:int,y:int,color:tuple[int,int,int],uid:int,token:str) -> str:
+async def paint(x:int,y:int,color:tuple[int,int,int],uid:int,token:str) -> tuple[int, list[int]]:
     id = random.randint(0,65535)
-    text = "\xfe"+int2str(x,2)+int2str(y,2)+int2str(color[0])+int2str(color[1])+int2str(color[2])+int2str(uid,3)+token2str(token)+int2str(id,4)
+    text = [0xfe]+int2list(x,2)+int2list(y,2)+int2list(color[0])+int2list(color[1])+int2list(color[2])+int2list(uid,3)+token2str(token)+int2list(id,4)
     return id,text
 # 00000000-0000-0000-0000-000000000000
 
-print(1)
+
+async def bytes2int(raw:bytes,st:int,ed:int):
+    num:int = 0
+    for i in range(0,ed-st):
+        num+=raw[i+st]*(1<<(8*i))
+    return num
+
+
+async def set_col(raw:bytes):
+    x = await bytes2int(raw,1,3)
+    y = await bytes2int(raw,3,5)
+    r = await bytes2int(raw,5,6)
+    g = await bytes2int(raw,6,7)
+    b = await bytes2int(raw,7,8)
+
+    # 在这里写你自己的处理方法
+    ...
+
+async def pt_res(raw:bytes,ress:dict[int,int]|None=None):
+    id = await bytes2int(raw,1,5)
+    op = await bytes2int(raw,5,6)
+    op_text = ""
+    if not ress is None:
+        ress[id]=op
+    match op:
+        case 0xef:
+            op_text = "Successful."
+        case 0xee:
+            op_text = "Too fast."
+        case 0xed:
+            op_text = "Token error."
+        case 0xec:
+            # 这本来是格式错误的，但是格式错误会直接掐连接
+            op_text = ""
+        case 0xeb:
+            op_text = "Promission error."
+        case 0xea:
+            op_text = "Server error."
+        case _:
+            raise TypeError(f"op {op} not found.")
+    
+    print(id,op,op_text)
+
+    
+    # 在这里写你自己的处理方法
+    ...
--- ws_client.py
+++ ws_client.py
@@ -7,21 +7,21 @@ import lib as mr
 resp:dict[int,str] = {}
 
 async def send_pt(ws_c):
+    # 这是一个向全版随机 #39C5BB 的 sample
     while True:
         try:
             x=random.randint(0,1000)
             y=random.randint(0,600)
-            id,data = mr.paint(0,0,(0x39,0xc5,0xbb),710036,"2f78a537-55ae-414a-8c76-1f679dbdafd0")
-            print(ord(data[0]))
-            await ws_c.send(data)
-            await asyncio.sleep(0.1)
+            id,data = await mr.paint(x,y,(0x39,0xc5,0xbb),710036,"2f78a537-55ae-414a-8c76-1f679dbdafd0")
+            await ws_c.send(bytes(data))
+            await asyncio.sleep(0.01)
         except Exception as ex:
             print(str(ex))
 
 
 async def connect(url:str):
     async with websockets.connect(url) as ws_c:
-        # asyncio.gather(send_pt(ws_c))
+        asyncio.gather(send_pt(ws_c))
         while True:
             res = await ws_c.recv()
             unp = await mr.unpack(res)
@@ -30,8 +30,10 @@ async def connect(url:str):
                     case 0xfc:
                         await ws_c.send(await mr.pong())
                         print("Pong!")
-                        continue
-                        # set_color
+                    case 0xff:
+                        await mr.pt_res(pkg)
+                    case 0xfa:
+                        await mr.set_col(pkg)
                     
                 
 asyncio.get_event_loop().run_until_complete(connect("wss://api.paintboard.ayakacraft.com:32767/api/paintboard/ws"))
